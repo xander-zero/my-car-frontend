fda4f1816106e77bc4c5f2f114332f3ff1b5000c
xander-zero <mahmud.ansari60636@gmail.com>
2023-01-27 13:41:01 +0330

    setup:setup frontend project

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+import styled from "styled-components";
+import tw from "twin.macro";
+
+import { HomePage } from "./app/containers/HomePage";
+
+import "./App.css";
+
+const AppContainer = styled.div`
+  ${tw`
+      w-full
+      h-full
+      flex
+      flex-col
+  `}
+`;
+
+function App() {
+  return (
+    <AppContainer>
+      <HomePage />
+    </AppContainer>
+  );
+}
+
+export default App;
